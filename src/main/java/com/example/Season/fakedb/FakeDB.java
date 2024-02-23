351c6b2712278ae5c9d211912b8e29b4be59da38
seb <null@null.fr>
2024-02-23 11:14:56 +0100

    Init 3.1

---
+++ src/main/java/com/example/Season/fakedb/FakeDB.java
@@ -0,0 +1,11 @@
+package com.example.Season;
+
+import com.example.Season.dto.UserDTO;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class FakeDB {
+    Map<Integer, UserDTO> db = new HashMap<>();
+}
