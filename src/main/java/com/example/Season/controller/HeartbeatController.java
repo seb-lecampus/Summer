d18f12a4b2d2dc4e740a46742a905a899554a05a
seb <null@null.fr>
2024-02-27 16:54:54 +0100

    add heartbeat profile

--- src/main/java/com/example/Season/controller/HeartbeatController.java
+++ src/main/java/com/example/Season/controller/HeartbeatController.java
@@ -2,12 +2,14 @@ package com.example.Season.controller;
 
 import com.example.Season.service.HeartbeatSensor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
 
 @RestController
+@Profile("heartbeat")
 public class HeartbeatController {
     private Random rng = new Random();
     @Autowired
