2a54bc2f52864288dccb0ae3706780f6e016244b
seb <null@null.fr>
2024-03-01 13:44:55 +0100

    Add JWT token generation

--- src/main/java/com/example/Season/controller/AuthenticationController.java
+++ src/main/java/com/example/Season/controller/AuthenticationController.java
@@ -1,6 +1,9 @@
 package com.example.Season.controller;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.Authentication;
@@ -8,19 +11,43 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+
 @RestController
 public class AuthenticationController {
     @Autowired
     AuthenticationConfiguration authenticationConfiguration;
 
     @PostMapping("api/public/login")
-    public AuthenticationResult login(@RequestBody AuthenticationParams authenticationParams){
+    public ResponseEntity<String> login(@RequestBody AuthenticationParams authenticationParams){
         try {
             Authentication authenticationRequest =
                 UsernamePasswordAuthenticationToken.unauthenticated(authenticationParams.username(), authenticationParams.password());
 
             Authentication authenticationResponse =
                     authenticationConfiguration.getAuthenticationManager().authenticate(authenticationRequest);
+
+            if(authenticationResponse.isAuthenticated()) {
+                var c = Calendar.getInstance();
+                c.add(Calendar.DAY_OF_MONTH, 1);
+
+                String jwtToken = Jwts.builder()
+                        .claim("name", authenticationParams.username)
+                        //.claim("authorities", TROLOLOL)
+                        .setSubject(authenticationParams.username)
+                        //.setId(UUID.randomUUID().toString())
+                        //.setIssuedAt(Date.from(now))
+                        .setExpiration(c.getTime())
+                        .compact();
+                var h = new HttpHeaders();
+                        h.set(HttpHeaders.AUTHORIZATION, jwtToken);
+                return ResponseEntity.ok()
+                        .headers(h)
+                        .body("");
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
